efb6e556521d1194a890d9cc2cdef4d4e2472980
Bartłomiej Gajda <b.gajda@webard.me>
2024-03-05 14:17:28 +0100

    wip

--- src/Services/ZadarmaService.php
+++ src/Services/ZadarmaService.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 namespace Webard\NovaZadarma\Services;
 
-use Illuminate\Support\Facades\Cache;
 use Zadarma_API\Api;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Cache;
 
 class ZadarmaService
 {
@@ -25,7 +26,13 @@ class ZadarmaService
     {
         // Must be cached due to Zadarma API limits
         return Cache::remember('nova-zadarma-key2-'.$sip, 60, function () use ($sip) {
-            return $this->api->getWebrtcKey($sip)->key;
+            try {
+                return $this->api->getWebrtcKey($sip)->key;
+            } catch (\Exception $e) {
+                Log::error('Zadarma Webrtc Key Error for SIP '.$sip.':', $e->getMessage());
+                return null;
+            }
+            
         });
     }
 
